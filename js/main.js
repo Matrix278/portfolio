46df33f320733d2ff6369fcf070a7721e0d3f5b5
Martin Sidorov <martin.sidorov27@gmail.com>
2021-04-11 01:19:53 +0300

    Delete unnecessary js and css files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//------------------------NAVBAR ACTIVE LINKS------------------------//
+$(".nav-link").on("click", function (e) {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+});
+
+//------------------------SCROLL TO TOP------------------------//
+var button = document.getElementById("scrollToTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
